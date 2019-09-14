f0f1f96deb31e62abca34c40ccc5a8abd693f5da
Panfilov17 <rikko.alexandr@gmail.com>
2019-09-15 01:51:57 +0300

    add rating

--- src/components/ShowPage.vue
+++ src/components/ShowPage.vue
@@ -6,16 +6,18 @@
 
         <img :src="serial.img" alt="" class="out_box_img">
 
-        <div class="out_box_rating">
+        <!--<div class="out_box_rating">
           <p v-if="serial.rating" class="out_box_rating_text">РЕЙТИНГ : {{serial.rating}}</p>
           <p v-else class="out_box_rating_text">РЕЙТИНГ : 0.0</p>
+        </div>-->
+        <div class="rating">
+          <div class="stars"></div>
+          <div id="rating" class="yellow_stars"></div>
         </div>
-        <div class="out_box_link">
-          <a :href="serial.link" class="">САЙТ</a>
-        </div>
+        <a id="link" :href="serial.link" class="out_box_link">САЙТ</a>
       </div>
 
-      <div class="in_box">
+      <div id="inBox" class="in_box">
         <h1 class="in_box_title">{{serial.name}}</h1>
         <div class="in_box_status">СТАТУС : {{serial.status}}</div>
         <div v-if="serial.descr" v-html="serial.descr" class="in_box_descr"></div>
@@ -30,10 +32,28 @@
 <script>
 
 export default {
+  data () {
+    return {
+    }
+  },
   props: ['id'],
   methods: {
     back () {
       this.$router.go(-1)
+    }
+  },
+  mounted () {
+    let rating = this.serial.rating
+    let link = this.serial.link
+    if (link === null) {
+      let linkBox = document.getElementById('link')
+      linkBox.classList = 'out_box_no_link'
+      linkBox.textContent = 'НЕТ ССЫЛКИ'
+    }
+    if (rating !== null) {
+      let stars = document.getElementById('rating')
+      let ratingWidth = rating.toString().split('.').join('') + '%'
+      stars.style.width = ratingWidth
     }
   },
   computed: {
@@ -62,6 +82,21 @@ export default {
       margin: 15px 10px 0 10px
       width: 300px
       height: 500px
+      .rating
+        width: 270px
+        position: relative
+        display: block
+        margin: 15px auto 0 auto
+        .stars
+          height: 25px
+          width: 100%
+          background: url("../assets/stars/star4.png")
+        .yellow_stars
+          top: 0
+          position: absolute
+          height: 25px
+          width: 0%
+          background: url("../assets/stars/yellow_star.png")
       &_img
         border: 10px solid white
         margin-top: 15px
@@ -70,23 +105,30 @@ export default {
         -webkit-border-radius: 15px
         -moz-border-radius: 15px
         border-radius: 15px
-      &_rating
-        font-weight: lighter
-        margin: 15px 0 0 0
       &_link
         display: block
-        margin: 0 auto
-        background-color: $btn_shadow
-        height: 35px
-        width: 200px
+        margin: 15px auto 0 auto
+        padding: 10px
         border-radius: 10px
-        a
-          display: block
-          margin: 0 auto
-          padding-top: 5px
-          color: white
-          text-align: center
-          font-weight: lighter
+        background-color: $btn_shadow
+        width: 200px
+        color: white
+        text-decoration: none
+        text-align: center
+        font-weight: lighter
+      &_no_link
+        display: block
+        margin: 15px auto 0 auto
+        padding: 10px
+        border-radius: 10px
+        background-color: #485460
+        width: 200px
+        color: white
+        text-decoration: none
+        text-align: center
+        font-weight: lighter
+
+
       &_btn
         display: block
         margin: 0 auto
@@ -125,6 +167,6 @@ export default {
   @media(max-width: 660px)
 
     .in_box
-      margin: 15px 15px  0 15px!important
+      margin: 30px 15px  0 15px!important
       text-align: center!important
 </style>
